9f935e0f3ae7988e7dd75ad7bce089edb10ebd39
gladreddy <kikast@yandex.ru>
2022-11-23 16:05:21 +0300

    Start develop

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import styles from './app.module.css'; 
+
+
+App.propTypes = {
+  
+};
+
+function App(props) {
+  return (
+    <div className={styles.wrapper}>
+      
+    </div>
+  );
+}
+
+export default App;
