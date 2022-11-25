3f04f6f12a4a599ccb948eca2028fb6dc17c766e
gladreddy <kikast@yandex.ru>
2022-11-25 12:15:27 +0300

    burger constructor done

--- src/components/burger-constructor/BurgerConstructor.js
+++ src/components/burger-constructor/BurgerConstructor.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ConstructorElement, Button, DragIcon, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './b-constructor.module.scss'; 
 
@@ -7,8 +7,67 @@ import data from '../../utils/data';
 
 
 const BurgerConstructor = (props) => {
-    const bun = data.find(el => el._id = '60666c42cc7b410027a1a9b1' ) ;
+    const bun = data.find(el => el.name == 'Краторная булка N-200i' );
+
+    const ingredients = [
+        'Говяжий метеорит (отбивная)',
+        'Соус Spicy-X',
+        'Биокотлета из марсианской Магнолии',
+        'Соус традиционный галактический',
+        'Хрустящие минеральные кольца',
+        'Хрустящие минеральные кольца',
+        'Соус фирменный Space Sauce'
+    ];
+
+
     return (
+        <section className={styles.wrap}>
+           <ConstructorElement
+                type="top"
+                isLocked={true}
+                text={`${bun.name} (верх)`}
+                price={bun.price}
+                thumbnail={bun.image}
+                extraClass={styles.item}
+            />
+
+            <ul className={styles.list}>
+                {ingredients.map((ingredient, index) => {        
+                    const item = data.find(el => el.name == ingredient );  
+                    return (
+                        <li className={styles.item} key={index}>    
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                type="top"
+                                text={item.name}
+                                price={item.price}
+                                thumbnail={item.image}
+                            />
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <ConstructorElement
+                type="bottom"
+                isLocked={true}
+                text={`${bun.name} (низ)`}
+                price={bun.price}
+                thumbnail={bun.image}
+                extraClass={styles.item}
+            />
+
+            <div className={styles.checkout}>
+                 <div className="text text_type_digits-medium">
+                    610 
+                    <CurrencyIcon type="primary" />
+                 </div>
+                 <Button htmlType="button" type="primary" size="large" extraClass="ml-10">
+                    Оформить заказ
+                 </Button>
+            </div>
+
+        </section>
     );
 }
 
